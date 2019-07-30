efdfa3b9681e9a16c02cbf93a04d47b92e1cb153
Taylor Johndrew <johndrew.t@gmail.com>
2019-07-30 10:43:26 -0700

    refactor(App): Reorganized express

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # johndrew-out-of-milk
 Copy of the Android App: Out of Milk
+
+## Goals
+* track and store items purchases at stores over time
+* optimize for lowest prices and the fewest stores as possible
+* offer insight into purchasing history and trends
+
+## Overview / Background
+tbd
+
+## Architecture
+tbd
--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+function createServer() {
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json({ type: 'application/*+json' }))
+  const app = express();
 
-app.use('/', indexRouter);
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(bodyParser.json({ type: 'application/*+json' }))
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  app.use('/', indexRouter);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = app;
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  return app;
+}
+
+module.exports = createServer;
