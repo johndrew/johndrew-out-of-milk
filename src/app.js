14b6056cf7b943b6d63b19f4c01f56d49fb3c982
Taylor Johndrew <johndrew.t@gmail.com>
2019-08-02 11:10:21 -0700

    feat(Clinet): Added react boilerplate

--- src/app.js
+++ src/app.js
@@ -15,6 +15,11 @@ function createServer() {
       hello: String
       items: [Item]
     }
+
+    type Mutation {
+      addItem(name: String!): Item
+    }
+
     type Item {
       id: Int
       name: String
@@ -27,6 +32,9 @@ function createServer() {
       hello: () => 'Hello world!',
       items: () => db.Item.findAll(),
     },
+    Mutation: {
+      addItem: (name) => db.Item.create({ name }),
+    }
   };
   const server = new ApolloServer({ typeDefs, resolvers });
   const app = express();
@@ -38,6 +46,12 @@ function createServer() {
   app.use(cookieParser());
   app.use(bodyParser.json({ type: 'application/*+json' }));
   server.applyMiddleware({ app });
+
+  // add client routes
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  })
 
   // catch 404 and forward to error handler
   app.use(function (req, res, next) {
--- src/domains/items/items.integ.spec.js
+++ src/domains/items/items.integ.spec.js
@@ -1,7 +1,7 @@
 const { request } = require('../../test/testApi');
 
 describe('Items Integration Tests', () => {
-    describe('Given Items in the databse', () => {
+    describe('Given Items in the database', () => {
         describe('When all items are queried', () => {
             it('Then it should return them', async () => {
                 const query = `{
@@ -16,6 +16,36 @@ describe('Items Integration Tests', () => {
                     .then((response) => {
                         const actual = response.body.data.items;
                         expect(actual.length).toEqual(0);
+                    });
+            });
+        });
+
+        xdescribe('When adding an item', () => {
+            it('Then it should return the new item', () => {
+                const query = `{
+                    items {
+                        id
+                        name
+                    }
+                }`;
+                const mutation = `{
+                    mutation AddItems {
+                        addItem(name: "Soy Sauce") {
+                            id
+                            name
+                        }
+                    }
+                }`;
+                return request()
+                    .post('/graphql')
+                    .set('Content-Type', 'application/json')
+                    .set('Accept', 'application/json')
+                    .send({ query: mutation })
+                    .then((response) => {
+                        console.log(response.error);
+                        console.log(response.body);
+                        const actual = response.body.data;
+                        expect(actual).toBeTruthy();
                     });
             });
         });
